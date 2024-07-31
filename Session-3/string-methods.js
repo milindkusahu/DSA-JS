2ae287c4b69e201059c40631f85db3a1ec778bbd
milindkusahu <milindsahu2000@gmail.com>
2024-08-01 02:39:17 +0530

    capitalise problem

--- Session-3/string-methods.js
+++ Session-3/string-methods.js
@@ -17,3 +17,30 @@ console.log(str.indexOf("z")); // Output: -1 (not present)
 
 // sort()
 console.log(str.split("").sort().join("")); // Output: dehllloorw
+
+// String Usecases
+/*
+--> Store Name
+--> Google Search
+--> Messages
+--> Emails
+*/
+
+// Common Terminologies
+
+// Substring -> Example: ello-wo
+
+// Subsequence -> Example: elow (must be in sequence)
+
+// Prefix -> Example: h, hel, hello, hello world
+
+// Suffix -> Example: d, ld, rld, orld, hello world
+
+// Reversing a string
+// let str = "hello world";
+let newStr = "";
+
+for (let i = str.length - 1; i >= 0; i--) {
+  newStr = newStr + str[i];
+}
+console.log(newStr);

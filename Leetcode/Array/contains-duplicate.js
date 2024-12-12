485bd416842cfb58d7f3f658fee5ddb20c0d9e99
milindkusahu <milindsahu2000@gmail.com>
2024-12-12 20:09:17 +0530

    Contains duplicate and Matrix Sum

---
+++ Leetcode/Array/contains-duplicate.js
@@ -0,0 +1,45 @@
+/*
+
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+
+Output: true
+
+Explanation:
+
+The element 1 occurs at the indices 0 and 3.
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+
+Output: false
+
+Explanation:
+
+All elements are distinct.
+
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+
+Output: true
+
+ */
+
+var containsDuplicate = function (nums) {
+  let set = new Set();
+
+  for (let num of nums) {
+    if (set.has(num)) return true; // First check if number exists
+    set.add(num); // If not, then add it
+  }
+  return false;
+};
+
+console.log(containsDuplicate([1, 2, 3, 1])); // true
+console.log(containsDuplicate([1, 2, 3, 4])); // false
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])); // true
